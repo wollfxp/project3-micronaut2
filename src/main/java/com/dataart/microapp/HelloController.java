62501bfb39a052424c95d5bbfc0dce9bbc448ec2
wollfxp <wollfxp@gmail.com>
2020-08-21 13:59:46 +0300

    added "name" service endpoint

--- src/main/java/com/dataart/microapp/HelloController.java
+++ src/main/java/com/dataart/microapp/HelloController.java
@@ -4,6 +4,10 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import java.time.LocalDateTime;
 
 @Controller("/service")
@@ -12,6 +16,16 @@ public class HelloController {
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
         return "hello @ " + LocalDateTime.now();
+    }
+
+    @GET
+    @Path("{name}")
+    @Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+    public String customizedHello(@PathParam("name") String name) {
+        if (name == null) {
+            return "not hello >_>";
+        }
+        return "hello " + name + " @ " + LocalDateTime.now();
     }
 
 }
